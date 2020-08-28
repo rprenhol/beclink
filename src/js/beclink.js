d528f564fa22f9b8b4a6e8faefd2431bb6c10bb8
Rafael Prenholato <rafaelp@icmc.usp.br>
2020-08-28 01:31:20 -0300

    Manipulando links de Grupos, Classes e Materiais

--- src/js/beclink.js
+++ src/js/beclink.js
@@ -7,4 +7,25 @@ document.addEventListener('DOMNodeInserted', function() {
             items[i].setAttribute('href',(link.split(",")[4]).replace(/\"/g,''));
         }
     }
+    
+    var secoes = ['Grupo','Classe','Material'];
+
+    for(var i = 0; i < secoes.length; i++) {
+        let links = document.querySelectorAll('#ContentPlaceHolder1_gvFiltro' + secoes[i] + ' a');
+        for(var j = 0; j < links.length; j++) {
+            links[j].addEventListener('mousedown', targetBlank);
+        }
+    }
+
+    function targetBlank(e) {
+        if(e.button == 1) {
+            document.forms['form1'].setAttribute('target', '_blank');
+        }
+    }
 });
+
+
+
+
+// https://www.bec.sp.gov.br/BEC_Catalogo_ui/CatalogoPesquisa3.aspx?chave=&pesquisa=Y&cod_id=&ds_item=papel
+// https://www.bec.sp.gov.br/BEC_Catalogo_ui/CatalogoPesquisa3.aspx?chave=&pesquisa=Y&cod_id=&ds_item=papel
